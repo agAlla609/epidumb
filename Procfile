ac4968a72f5421394be5bd0fe789693ba09d21cf
Ian Agala <ian.b.agala@gmail.com>
2024-06-10 11:24:19 +0300

    Deploying To Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jangoblog.wsgi --log-file
---
+++ staticfiles/js/base.js
@@ -0,0 +1,5 @@
+const infoUpdate = document.querySelector('.update-btn')
+const updateForm = document.querySelector('.update')
+infoUpdate.addEventListener('click',()=> {
+    updateForm.classList.toggle('visible')
+})
